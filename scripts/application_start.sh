da4c8efda12cefcde3a72d4529836fe06dc704b4
Chirag Soni <chiragsoni81245@gmail.com>
2021-10-03 02:28:00 +0530

    Code Deploye Scripts Updates

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ubuntu/express-app
+
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/express-app
 
