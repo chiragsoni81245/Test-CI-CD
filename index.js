65541637ca088c2dd9dcf798c47e4fa2237a6698
Chirag Soni <chiragsoni81245@gmail.com>
2021-10-02 20:59:52 +0530

    Added Test Cases

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var express = require('express');
 
 var app = express();
 app.get('/', function (req, res) {
-	  res.send('Hello World!');
+	  res.send({msg: 'Hello World!'});
 });
 
 app.listen(process.env.PORT || 3000, function () {
 	  console.log('Example app listening on port 3000!');
 });
+
+
+module.exports = app; // for testing
---
+++ test/apitest.js
@@ -0,0 +1,22 @@
+process.env.NODE_ENV = 'test';
+
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../index");
+let should = chai.should();
+chai.use(chaiHttp);
+
+describe("APIS", () => {
+    describe("GET /", () => {
+        it("It should GET home page message", (done) => {
+        chai.request(server)
+        .get("/")
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('msg').eql('Hello World!');
+                done();
+            });
+        });
+    });
+})
