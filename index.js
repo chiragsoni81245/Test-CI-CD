2a6943f57e4c79796b2d7507775210cc0b4bc0e1
Chirag Soni <chiragsoni81245@gmail.com>
2021-10-03 02:32:08 +0530

    Udated Index.js file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function () {
-	  console.log('Example app listening on port 3000!');
+	  console.log('Example app listening on port 3000! Yo its Fun');
 });
 
 
