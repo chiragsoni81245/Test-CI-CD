73f84b9fcab7c55d05c6ea4f061f709e2aec0a80
Chirag Soni <chiragsoni81245@gmail.com>
2021-10-03 02:44:27 +0530

    Updated before install script for aws code deploy'

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#download node and npm
-sudo apt update
-sudo apt install nodejs npm -y 
+# #download node and npm
+# sudo apt update
+# sudo apt install nodejs npm -y 
 
-#create our working directory if it doesnt exist
-DIR="/home/ubuntu/express-app"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
+# #create our working directory if it doesnt exist
+# DIR="/home/ubuntu/express-app"
+# if [ -d "$DIR" ]; then
+#   echo "${DIR} exists"
+# else
+#   echo "Creating ${DIR} directory"
+#   mkdir ${DIR}
+# fi
 
-exit 1
+mkdir /home/123/123
