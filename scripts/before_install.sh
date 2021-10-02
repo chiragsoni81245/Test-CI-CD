05123253754d77bc8661e5e3bd30861041038f5f
Chirag Soni <chiragsoni81245@gmail.com>
2021-10-03 01:22:50 +0530

    Code Deploye Scripts Updates

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+sudo apt update
+sudo apt install nodejs npm
 
 #create our working directory if it doesnt exist
 DIR="/home/ubuntu/express-app"
