a29a82720b10cf5fa89e7e9026d3a7e2f73a8892
Chirag Soni <chiragsoni81245@gmail.com>
2021-12-05 16:59:50 +0530

    Removed GitHub Actions Workflow

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require('express');
 
 var app = express();
 app.get('/', function (req, res) {
-	  res.send({msg: 'Hello World!!', statusCode: 200});
+	res.send({
+		msg: 'Hello World, its working!!', 
+		statusCode: 200
+	});
 });
 
 app.listen(process.env.PORT || 3000, function () {
